e75ff762fce04db4ab94c6398d33deb81f9449e9
cas <cas@interglobal.vision>
2017-08-10 16:26:46 -0500

    add muted property to videos

--- partials/splash-video.php
+++ partials/splash-video.php
@@ -8,10 +8,10 @@ if (!empty($video) && !empty($video_reversed)) {
 
 <div id="splash-video-container" class="padding-top-large margin-top-large grid-row">
   <div class="grid-item item-s-12">
-    <video id="splash-video-1" class="splash-video" preload width="100%">
+    <video id="splash-video-1" class="splash-video" preload width="100%" muted>
       <source src="<?php echo $video;?>" type="video/mp4">
      </video>
-    <video id="splash-video-2" class="splash-video u-hidden" preload width="100%">
+    <video id="splash-video-2" class="splash-video u-hidden" preload width="100%" muted>
       <source src="<?php echo $video_reversed;?>" type="video/mp4">
      </video>
   </div>
