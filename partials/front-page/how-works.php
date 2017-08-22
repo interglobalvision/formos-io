0356be90a98cc500a5d4f61f2f545b2dae08e14f
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-08-22 14:59:54 -0500

    include svgs in markup. add missing sections

--- partials/front-page/how-works.php
+++ partials/front-page/how-works.php
@@ -6,21 +6,21 @@
   </div>
   <div class="grid-row align-items-end justify-between text-align-center">
     <div class="grid-item">
-      <?php // SVG 1 ?>
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_1.svg'); ?>
     </div>
     <div class="grid-item">
-      <?php // SVG 2 ?>
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_2.svg'); ?>
       Download into your own device
     </div>
     <div class="grid-item item-s-3">
-      <?php // SVG 3 ?>
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_3.svg'); ?>
     </div>
     <div class="grid-item">
-      <?php // SVG 4 ?>
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_4.svg'); ?>
       Wireless communication
     </div>
     <div class="grid-item item-s-3">
-      <?php // SVG 5 ?>
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_5.svg'); ?>
     </div>
   </div>
   <div class="grid-row justify-between text-align-center">
@@ -32,6 +32,46 @@
     </div>
     <div class="grid-item item-s-3">
       formOS units
+    </div>
+  </div>
+</div>
+
+<div class="container padding-bottom-basic">
+  <div class="grid-row">
+    <div class="grid-item item-s-12 text-align-center margin-bottom-small">
+      <h3 class="font-size-basic">A formOS unit has 6 connecting sides</h3>
+    </div>
+  </div>
+  <div class="grid-row">
+    <div class="grid-item item-s-12 text-align-center">
+      // 6 sides image
+    </div>
+  </div>
+</div>
+
+<div class="container padding-bottom-basic">
+  <div class="grid-row">
+    <div class="grid-item item-s-12 text-align-center margin-bottom-small font-size-basic">
+      <h3>formOS can be connected in an infinite amount of ways</h3>
+      <span>(here are a few basic ones)</span>
+    </div>
+  </div>
+  <div class="grid-row">
+    <div class="grid-item item-s-12 item-m-3 text-align-center">
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_sphere.svg'); ?>
+      <h4>Sphere</h4>
+    </div>
+    <div class="grid-item item-s-12 item-m-3 text-align-center">
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_line.svg'); ?>
+      <h4>Line</h4>
+    </div>
+    <div class="grid-item item-s-12 item-m-3 text-align-center">
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_arc.svg'); ?>
+      <h4>Arc</h4>
+    </div>
+    <div class="grid-item item-s-12 item-m-3 text-align-center">
+      <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_icon_more.svg'); ?>
+      <h4>And many more...</h4>
     </div>
   </div>
 </div>
