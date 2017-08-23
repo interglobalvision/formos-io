af0b0f0ae084d1ad487818f755deb6a53a3d98d8
Patrick Best <patrickbest@patrickbest.com>
2017-08-23 12:43:25 +0100

    revise down from bad pt values to sensible

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@ get_header();
 
   <div class="container padding-bottom-basic">
     <div class="grid-row">
-      <div class="grid-item item-s-12 text-align-center">
+      <div class="grid-item item-s-12 item-l-8 offset-l-2 text-align-center">
         <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/formos_logo.svg'); ?>
 
         <h2 class="font-size-extra">The first modular entertainment system.</h2>
@@ -18,7 +18,7 @@ get_header();
 
   <div class="container padding-bottom-basic">
     <div class="grid-row">
-      <div class="grid-item item-s-12 item-m-10 offset-m-1 text-align-center">
+      <div class="grid-item item-s-12 item-m-10 offset-m-1 item-l-8 offset-l-2 text-align-center">
         <h2 class="font-size-extra font-uppercase js-fix-widows">Play with the power of nature</h2>
         <h3 class="font-size-large js-fix-widows">Enter formOS, an entertainment system inspired by the modular qualities of Nature — a uniquely powerful gaming experience.</h3>
       </div>
@@ -27,7 +27,7 @@ get_header();
 
   <div class="container padding-bottom-basic">
     <div class="grid-row">
-      <div class="grid-item item-s-12 item-m-10 offset-m-1 text-align-center">
+      <div class="grid-item item-s-12 item-m-10 offset-m-1 item-l-8 offset-l-2 text-align-center">
         <h2 class="font-size-mid js-fix-widows">But what exactly is formOS</h2>
         <h3 class="font-size-basic js-fix-widows">formOS is a modular gaming system that integrates physical and digital play. formOS modules have the capacity to connect to each other and to communicate to mobile devices.</h3>
       </div>
