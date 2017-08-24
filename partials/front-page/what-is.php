7ee9644fff25f7af099defd3464e432d1504b27b
Patrick Best <patrickbest@patrickbest.com>
2017-08-24 18:48:02 +0100

    move to partial

---
+++ partials/front-page/what-is.php
@@ -0,0 +1,18 @@
+<div id="section-what-is" class="container padding-bottom-basic">
+  <div class="grid-row">
+    <div class="grid-item item-s-12 item-m-10 offset-m-1 item-l-8 offset-l-2 text-align-center">
+      <h2 class="font-size-mid js-fix-widows">But what exactly is formOS</h2>
+      <h3 class="font-size-basic js-fix-widows">formOS is a modular gaming system that integrates physical and digital play. formOS modules have the capacity to connect to each other and to communicate to mobile devices.</h3>
+    </div>
+  </div>
+</div>
+
+<div class="container padding-bottom-basic">
+  <div class="grid-row">
+    <div class="grid-item item-s-12 text-align-center">
+      <h2 class="font-size-basic js-fix-widows">See formOS in action:</h2>
+      //*** longer video
+      <video></video>
+    </div>
+  </div>
+</div>
