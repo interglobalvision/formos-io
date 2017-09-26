92e50df2bf76f486cc2f9d88ccb618591febee48
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-09-26 16:58:51 -0500

    style for vimeo

--- partials/front-page/long-video.php
+++
@@ -1,9 +0,0 @@
-<div class="container padding-bottom-basic">
-  <div class="grid-row">
-    <div class="grid-item item-s-12 text-align-center">
-      <h2 class="font-size-basic js-fix-widows">See formOS in action:</h2>
-      //*** longer video
-      <video></video>
-    </div>
-  </div>
-</div>
